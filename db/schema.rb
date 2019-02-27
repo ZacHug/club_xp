2ac80f873650bbcd68879649df98f6187c2478b7
Zachari Hughes <zac-hughes@outlook.com>
2019-02-27 16:38:01 -0500

    Added videos, relations, created video uploader file and created a video table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_200413) do
+ActiveRecord::Schema.define(version: 2019_02_27_212129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,18 @@ ActiveRecord::Schema.define(version: 2019_02_25_200413) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "club_id"
+    t.string "video"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["club_id"], name: "index_videos_on_club_id"
+    t.index ["user_id"], name: "index_videos_on_user_id"
+  end
+
   add_foreign_key "comments", "clubs"
   add_foreign_key "comments", "users"
+  add_foreign_key "videos", "clubs"
+  add_foreign_key "videos", "users"
 end
