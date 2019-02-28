3ea66904cf74326e5d2834a5c75c4c8eff49b829
Patrice Gedeon <pgedeon84@gmail.com>
2019-02-28 17:49:25 -0500

    coded footer, navbar, and CSS

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -6,13 +6,12 @@ class ClubsController < ApplicationController
     else
       @club = Club.near(params[:address])
     end
-
   end
 
   def show
     @club = Club.find(params[:id])
     @comment = Comment.new
     @comment.club = @club
-  end
 
+  end
 end
