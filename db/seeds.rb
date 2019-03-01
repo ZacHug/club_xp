371315d3c8d1d4f53203c12049e55311fa1fe027
nohat yacinthe <nohatyacinthe1@hotmail.fr>
2019-03-01 16:07:53 -0500

    chnages to seed and entrance

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,14 @@ puts "User"
   remote_photo_url: "https://s3-media3.fl.yelpcdn.com/bphoto/yooQiJ3Pe9i6h79lT0BgDw/ls.jpg"
   )
 
+ User.create!(
+  first_name: "Ryan",
+  last_name: "Howard",
+  email: "ryan@gmail.com",
+  password: "123456",
+  remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Kyrie_Irving_June_2016_crop.jpg/270px-Kyrie_Irving_June_2016_crop.jpg"
+  )
+
 puts "done User"
 
 
@@ -30,7 +38,7 @@ club1 = Club.create!(
     address: "2108 Blvd St-Laurent Montreal, QC H2X 2T2",
     remote_photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRblYbXP06x8Hfzzg6YQ5xmR9IVChDS9Vy2QnzGxScCWe1pTJv4",
     description: " Located inside Hotel10, Blvd44 is a popular Montreal nightclub with three distinct sections (main room, mezzanine and terrace), each offering its own unique atmosphere to please everyone. A spacious dance floor and numerous VIP booth await you at Blvd44, a popular and casual nightclub where you can socialize and have a drink with friends in private.",
-    price: "$20.00"
+    price: 20
   )
 
 club2 = Club.create!(
@@ -38,7 +46,7 @@ club2 = Club.create!(
     address: "3643 St-Laurent, APT.200 Montreal, QC",
     remote_photo_url: "https://images.unsplash.com/photo-1541057591728-77510a9ea77f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
     description: "Buzzy, brick-lined bar with hip, vintage decor, a pool table & arcade games, plus bottle service.",
-    price: "$15.00"
+    price: 20
   )
 
 club3 = Club.create!(
@@ -46,7 +54,7 @@ club3 = Club.create!(
     address: "3781 St Laurent Blvd, Montreal, QC H2W 1X8",
     remote_photo_url: "https://www.theplunge.com/wp-content/uploads/2017/07/travel_bachelor_party_montreal_best_nightclubs_stereo.jpg",
     description: "Posh nightspot featuring 2 dance floors with eclectic DJ–spun beats & a terrace.",
-    price: "$25.00"
+    price: 20
   )
 
 club4 = Club.create!(
@@ -54,7 +62,7 @@ club4 = Club.create!(
     address: "858 St Catherine St E, Montreal, QC H2L 2E3",
     remote_photo_url: "https://media-cdn.tripadvisor.com/media/photo-s/06/f2/de/e8/stereo-nightclub.jpg",
     description: "Stereo nightclub is an afterhours club in Montreal, Quebec, Canada which primarily features house music and techno. The afterhours section of the club is always open on Fridays and Saturdays. Stereo nightclub was created by DJ/producer Angel Moraes and later co-owned by Tommy Piscardeli.",
-    price: "$10.00"
+    price: 20
   )
 
 club5 = Club.create!(
@@ -62,7 +70,7 @@ club5 = Club.create!(
     address: "3500 St Laurent Blvd, Montreal, QC H2X 2V2",
     remote_photo_url: "https://montrealx.com/wp-content/uploads/2017/06/best-top-newest-montreal-nightclubs-rooftop-bar-Somwhr-Liquor-Lounge-2017-Club-Ecole-Privee.jpg",
     description: "Dimly-lit nightspot with a classy, lounge vibe, featuring house dance music, DJs & bottle service.",
-    price: "$10.00"
+    price: 20
   )
 
 club6 = Club.create!(
@@ -70,7 +78,7 @@ club6 = Club.create!(
     address: "151 Ontario St E, Montreal, QC H2X 1H5",
     remote_photo_url: "https://lh4.googleusercontent.com/proxy/TemLhSEOrKdm3WYxt6apiFMSfqwaexu0ShxObPeXaFtGBssP1QoOcthntpimXJtM4DTqRuxuY4Ho5mYb_mUBBPkhq6eBLLxeDnYYwfrUZiiyw8686uhvHcTYMpYEFBvIIQ2XMdtKT9NAjOmzfGqoUAD2aXCT7Co=w99-h99-n-k-no",
     description: "Cosmopolitan nightspot with music & bottle service in a sophisticated atmosphere.",
-    price: "$10.00"
+    price: 20
   )
 
 club7 = Club.create!(
@@ -78,7 +86,7 @@ club7 = Club.create!(
     address: " 3556 St Laurent Blvd, Montreal, QC H2X 2V1",
     remote_photo_url: "http://www.barmontreal.com/wp-content/themes/corporate/images/tmp/ivy/ivypic.jpg",
     description: "Vibrant, over-18 nightspot open weekends only & featuring DJs, dancing & bars serving drinks.",
-    price: "$10.00"
+    price: 20
   )
 
 club8 = Club.create!(
@@ -86,7 +94,7 @@ club8 = Club.create!(
     address: "4372 St Laurent Blvd, Montreal, QC H2W 1Z5",
     remote_photo_url: "http://balattou.com/wp-content/uploads/2014/02/slider14-900x350.jpg",
     description: "Cozy nightspot with live world-beats performances & visiting DJs on weekend nights.",
-    price: "$10.00"
+    price: 20
   )
 
 club9 = Club.create!(
@@ -94,7 +102,7 @@ club9 = Club.create!(
     address: "4483 St Laurent Blvd, Montreal, QC H2W 1Z8",
     remote_photo_url: "http://www.lebelmont.com/v2/accueil.jpg",
     description: "High-energy hub for live acts, DJs & dancing, plus pool tables, improv nights & events.",
-    price: "$10.00"
+    price: 20
   )
 
 club10 = Club.create!(
@@ -102,7 +110,7 @@ club10 = Club.create!(
     address: "4465 St Laurent Blvd, Montreal, QC H2W 1Z8",
     remote_photo_url: "http://boulevardsaintlaurent.com/custom-content/uploads/2018/05/SalonDaome-Cover1.png",
     description: "Laid-back nightclub with a roster of international DJs spinning electronic music, plus theme nights.",
-    price: "$10.00"
+    price: 20
   )
 
 
@@ -119,20 +127,20 @@ Video.create!(
 )
 
 Video.create!(
-  club: club2,
+  club: club1,
   user: User.first,
   video: File.open("db/videos/02APT200.mp4")
 
 )
 
 Video.create!(
-  club: club3,
+  club: club1,
   user: User.first,
   video: File.open("db/videos/03MUZ.mp4")
 )
 
 Video.create!(
-  club: club4,
+  club: club1,
   user: User.first,
   video: File.open("db/videos/04SAH.mp4")
 )
