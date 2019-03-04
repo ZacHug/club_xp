86bdc708cd9a4511d254b97f6781a3d6cb280fab
Zachari Hughes <zac-hughes@outlook.com>
2019-03-04 14:27:42 -0500

    Major errors fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ puts 'Creating 10 fake user...'
     first_name:Faker::Name.name
     last_name:Faker::Name.name
     email:Faker::Email.email
-    password:
+    password:"123456"
   )
   user.save!
 end
